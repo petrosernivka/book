44768f590d9ef9b77d5ffcccd3522cc322a80fc3
petrosernivka <p.sernivka@gmail.com>
2018-12-06 19:19:53 +0200

    created class 'Post'

--- post/models.py
+++ post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    class Meta():
+        db_table = 'post'
+    post_title = models.CharField(max_length = 200)
+    post_text = models.TextField()
+    post_date = models.DateTimeField()
+    post_likes = models.IntegerField()
+    post_unlikes = models.IntegerField()
