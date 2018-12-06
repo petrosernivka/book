344c66303efd86f406a3a80822601f25a26325d4
petrosernivka <p.sernivka@gmail.com>
2018-12-06 22:35:39 +0200

    created class 'Comments'

--- post/models.py
+++ post/models.py
@@ -3,8 +3,17 @@ from django.db import models
 class Post(models.Model):
     class Meta():
         db_table = 'post'
+
     post_title = models.CharField(max_length = 200)
     post_text = models.TextField()
     post_date = models.DateTimeField()
-    post_likes = models.IntegerField()
-    post_unlikes = models.IntegerField()
+    post_likes = models.IntegerField(default=0)
+    post_unlikes = models.IntegerField(default=0)
+
+
+class Comments(models.Model):
+    class Meta():
+        db_table = 'comments'
+
+    comments_text = models.TextField()
+    comments_post = models.ForeignKey('Post', on_delete=models.PROTECT)
