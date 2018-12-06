b17f8aceee5fbd754953d3af51a974c636a23fb0
petrosernivka <p.sernivka@gmail.com>
2018-12-06 23:18:44 +0200

    added 'Post' to admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from post.models import Post, Comments
+
 
 # Register your models here.
+class PostInline(admin.StackedInline):
+    model = Comments
+    extra = 3
+
+
+class PostAdmin(admin.ModelAdmin):
+    exclude = ['post_likes', 'post_unlikes']
+    inlines = [PostInline]
+    list_filter = ['post_date']
+
+
+admin.site.register(Post, PostAdmin)
