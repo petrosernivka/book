b0867c8fc933553ffd811f1900fcede50f1a1ee4
petrosernivka <p.sernivka@gmail.com>
2018-12-07 13:47:10 +0200

    created postr.html

--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from post.models import Post, Comments
 
 # Create your views here.
+
+def posts(request):
+    return render(request, 'posts/posts.html', context={'posts': Post.objects.all()})
+
+def post(request, post_id=1):
+    return rendere(request, 'posts/post.html', context={'post': Post.objects.get(id=post_id), 'comments': Comments.objects.filter(comments_posts_id=post_id)})
