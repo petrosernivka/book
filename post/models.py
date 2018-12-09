af6b8e89416468c8c2a5453820f1c741bdc586c8
petrosernivka <p.sernivka@gmail.com>
2018-12-09 22:36:31 +0200

    added fild 'DateTime' to comments

--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     class Meta():
@@ -17,5 +18,6 @@ class Comments(models.Model):
     class Meta():
         db_table = 'comments'
 
+    comments_date = models.DateTimeField()
     comments_text = models.TextField(verbose_name="Текст коментаря:")
     comments_post = models.ForeignKey('Post', on_delete=models.PROTECT)
